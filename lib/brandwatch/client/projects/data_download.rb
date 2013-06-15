0e8fc11fc8f7d496e091551ece1232ef8531f330
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-06-15 17:44:56 +0100

    Added project data download module endpoint

---
+++ lib/brandwatch/client/projects/data_download.rb
@@ -0,0 +1,49 @@
+module Brandwatch
+  class Client
+    module Projects
+      module DataDownload
+
+        # Get all data downloads in project
+        #
+        # @param id [Integer] Id of project
+        # @option opts [Integer] :page Page of projects to retrieve
+        # @option opts [Integer] :pageSize Results per page of results
+        # @return [Hashie::Mash] All data downloads in project
+        def data_downloads id, opts={}
+          get "projects/#{id}/datadownload"
+        end
+
+        # Get a specific data download in project
+        #
+        # @param project_id [Integer] Id of project
+        # @param data_download_id [Integer] Id of data download
+        # @return [Hashie::Mash] Specific data download
+        def get_data_download project_id, data_download_id
+          get "projects/#{project_id}/datadownload#{data_download_id}"
+        end
+        alias :data_download :get_data_download
+
+        # Create a new data download in project
+        #
+        # @param id [Integer] Id of project
+        # @param opts [Hash] options hash of parameters
+        # @option opts [Integer] projectId Id of the project
+        # @option opts [Integer] id Id of the data download
+        # @option opts [Integer] queryId Id of the query
+        # @option opts [Int] percentComplete The percentage complete of data download
+        # @option opts [String] status The status of the download
+        # @option opts [String] queryName The query name of the data download
+        # @option opts [Date] endDate The end date of the data download
+        # @option opts [Date] requestDate The request date of the data download
+        # @option opts [String] downloadLinkXLS The link to download the XLS format
+        # @option opts [String] downloadLinkCSV The link to download the CSV format
+        # @option opts [Array] additionalColumns The additional columns for the data download
+        # @return [Hashie::Mash] New data download
+        def create_datadownload id, opts={}
+          get "projects/#{id}/datadownload"
+        end
+
+      end
+    end
+  end
+end
