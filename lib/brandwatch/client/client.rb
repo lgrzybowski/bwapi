c807ff48c2f14ec529552a5d2c47bcfb210deea0
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-06-15 23:56:37 +0100

    Added client module endpoint

---
+++ lib/brandwatch/client/client.rb
@@ -0,0 +1,22 @@
+module Brandwatch
+  class Client
+    module Client
+
+      # Get the current users client
+      #
+      # @return [Hashie::Mash] Client information
+      def client
+        get "client"
+      end
+
+      # Get the current users client modules
+      #
+      # @return [Hashie::Mash] Client module information
+      def client_modules
+        get "client/modules"
+      end
+      alias :modules :client_modules
+
+    end
+  end
+end
