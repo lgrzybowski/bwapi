39eeb79b7f2473c45a1375e594194edc243919fb
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-06-15 18:52:09 +0100

    Added projects data and queries mentions module endpoints

---
+++ lib/brandwatch/client/projects/data.rb
@@ -0,0 +1,62 @@
+module Brandwatch
+  class Client
+    module Projects
+      module Data
+
+        # Get all chart data for the requested dimensions
+        #
+        # @param id [Integer] Id of project
+        # @param aggregate [String] Aggregate value
+        # @param dimension_one [String] Dimension 1 value
+        # @param dimension_two [String] Dimension 2 value
+        # @param opts [Hash] options hash of parameters
+        # @option opts [Integer] projectId Id of the project
+        # @option opts [Hash] filter The filters to apply
+        # @return [Hashie::Mash] All Chart data mentions
+        def data id, aggregate, dimension_one, dimension_two, opts={}
+          get "projects/#{id}/#{aggregate}/#{dimension_one}/#{dimension_two}", opts
+        end
+
+        # Get all chart data for date ranges broken down by days
+        #
+        # @param id [Integer] Id of project
+        # @param opts [Hash] options hash of parameters
+        # @option opts [Integer] projectId Id of the project
+        # @option opts [Hash] filter The filters to apply
+        # @option opts [Array] dateRanges Date range ids
+        # @return [Hashie::Mash] All Chart data mentions
+        def data_date_ranges_days id, opts
+          get "projects/#{id}/data/volume/dataRanges/days", opts
+        end
+
+        # Get mentions that fall within the submitted filters
+        #
+        # @param id [Integer] Id of project
+        # @param opts [Hash] options hash of parameters
+        # @option opts [Integer] projectId Id of the project
+        # @option opts [Hash] filter The filters to apply
+        # @option opts [Integer] :page Page of projects to retrieve
+        # @option opts [Integer] :pageSize Results per page of results
+        # @option opts [String] :orderBy Parameter to sort by
+        # @option opts [String] :orderDirection Direction of sort
+        # @option opts [Integer] :orderByCategory Category to sort by when orderBy category
+        # @return [Hashie::Mash] All Mentions
+        def data_mentions id, opts={}
+          get "projects/#{id}/data/mentions", opts
+        end
+
+        # Update mentions for project
+        #
+        # @param id [Integer] Id of project
+        # @param opts [Hash] options hash of parameters
+        # @option opts [Integer] projectId Id of the project
+        # @option opts [Hash] List The patches to be applied
+        # @return [Hashie::Mash] Updated mentions
+        def update_data_mentions id, opts={}
+          patch "projects/#{id}/data/mentions", opts
+        end
+
+      end
+    end
+  end
+end
---
+++ lib/brandwatch/client/projects/queries/mentions.rb
@@ -0,0 +1,53 @@
+module Brandwatch
+  class Client
+    module Projects
+      module Queries
+        module Mentions
+
+          # Get the full text of query mention
+          #
+          # @param project_id [Integer] Id of project
+          # @param query_id [Integer] Id of query
+          # @param mention_id [Integer] Id of mention
+          # @return [Hashie::Mash] Content of query mention
+          def get_query_mention_content project_id, query_id, mention_id
+            get "projects/#{project_id}/queries/#{query_id}/mentions/#{mention_id}/content"
+          end
+          :query_mention_content :get_query_mention_content
+          :mention_content :get_query_mention_content
+
+          # Update query mention
+          #
+          # @param project_id [Integer] Id of project
+          # @param query_id [Integer] Id of query
+          # @param mention_id [Integer] Id of mention
+          # @param opts [Hash] options hash of parameters
+          # @option opts [Integer] projectId Id of the project
+          # @option opts [Integer] queryId Id of the query
+          # @option opts [Integer] id Id of the mention
+          # @option opts [Hash] MentionPatchDTO patch to be applied
+          # @return [Hashie::Mash] Updated query mention
+          def update_query_mention project_id, query_id, mention_id, opts
+            patch "projects/#{project_id}/queries/#{query_id}/mentions/#{mention_id}", opts
+          end
+
+          # Delete query mention
+          #
+          # @param project_id [Integer] Id of project
+          # @param query_id [Integer] Id of query
+          # @param mention_id [Integer] Id of mention
+          # @param opts [Hash] options hash of parameters
+          # @option opts [Integer] projectId Id of the project
+          # @option opts [Integer] queryId Id of the query
+          # @option opts [Integer] id Id of the mention
+          # @option opts [Hash] Map patch to be applied
+          # @return [Hashie::Mash] Deleted query mention
+          def delete_query_mention project_id, query_id, mention_id, opts
+            delete "projects/#{project_id}/queries/#{query_id}/mentions/#{mention_id}", opts
+          end
+
+        end
+      end
+    end
+  end
+end
