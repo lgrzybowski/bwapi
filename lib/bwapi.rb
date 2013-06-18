c39193afb61608d93a2182b4b1ea1e1420858fe9
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-06-18 05:39:22 +0100

    Added method_missing and respond_to? methods - can't really be used until Brandwatch has public endpoints as all require authentication

--- lib/bwapi.rb
+++ lib/bwapi.rb
@@ -8,8 +8,21 @@ module BWAPI
   extend Configuration
   class << self
 
+    # Alias for BWAPI::Client.new
+    #
+    # @return [BWAPI::Client]
     def new opts={}
       BWAPI::Client.new opts
+    end
+
+    # Delegate to BWAPI::Client.new
+    def method_missing method, *args, &block
+      return super unless new.respond_to? method
+      new.send method, *args, &block
+    end
+
+    def respond_to? method, include_private=false
+      new.respond_to? method, include_private || super(method, include_private)
     end
 
   end
