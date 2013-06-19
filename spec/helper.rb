997bfe9cb1591cf082dfd4a029887e20b1ebb71f
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-06-19 22:56:56 +0100

    Fixed spacing

--- spec/helper.rb
+++ spec/helper.rb
@@ -8,6 +8,6 @@ SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start
 
 RSpec.configure do |config|
-  config.color_enabled = true
-   config.formatter = :documentation
+  config.color_enabled  = true
+  config.formatter      = :documentation
 end
