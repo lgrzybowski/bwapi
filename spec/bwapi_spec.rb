72b942d7c6037c6c611bceeef3fec94595476258
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-06-19 22:39:11 +0100

    Updated bwapi spec tests, added helper and configured simplecov

--- spec/bwapi_spec.rb
+++ spec/bwapi_spec.rb
@@ -1,14 +1,22 @@
-require 'coveralls'
-Coveralls.wear!
+require 'helper'
 
-require 'brandwatch'
-require 'rspec'
+describe 'BWAPI' do
 
-describe Brandwatch do
+  describe 'when called' do
+    it 'should be an instance of Module' do
+      BWAPI.should be_an_instance_of Module
+    end
+  end
 
-  context "when called" do
-    it "should be an instance of Module" do
-      Brandwatch.should be_an_instance_of Module
+  describe '.new' do
+    it 'is a BWAPI::Client' do
+      expect(BWAPI.new).to be_a BWAPI::Client
+    end
+  end
+
+  describe '.respond_to?' do
+    it 'returns true if new method exists' do
+      expect(BWAPI.respond_to?(:new, true)).to eq(true)
     end
   end
 
--- spec/helper.rb
+++ spec/helper.rb
@@ -1,5 +1,13 @@
 require 'coveralls'
 require 'simplecov'
 
-SimpleCov.start
+require 'bwapi'
+require 'rspec'
+
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.color_enabled = true
+   config.formatter = :documentation
+end
