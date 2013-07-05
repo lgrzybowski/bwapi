bf6ff61f4e06335c58c98835721f8e5bf2bd0d70
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-07-05 13:02:43 +0100

    Removed SimpleCov and use Coveralls simple implementation instead

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,10 +1,6 @@
 require 'coveralls'
-require 'simplecov'
-
+Coveralls.wear!
 require 'bwapi'
-
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start
 
 RSpec.configure do |config|
   config.color_enabled  = true
