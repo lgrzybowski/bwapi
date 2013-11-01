b699a56204ddced73f13103c9358752b7020eb44
Jonathan Chrisp <jonathan@brandwatch.com>
2013-11-01 13:42:18 +0000

    Initial commit of the stream dashboards endpoints

--- lib/bwapi/client/projects.rb
+++ lib/bwapi/client/projects.rb
@@ -5,6 +5,7 @@ require 'bwapi/client/projects/facebook_queries'
 require 'bwapi/client/projects/signals'
 require 'bwapi/client/projects/summary'
 require 'bwapi/client/projects/sharing'
+require 'bwapi/client/projects/stream_dashboards'
 require 'bwapi/client/projects/queries'
 require 'bwapi/client/projects/query_groups'
 require 'bwapi/client/projects/tags'
@@ -82,6 +83,7 @@ module BWAPI
       include BWAPI::Client::Projects::Signals
       include BWAPI::Client::Projects::Summary
       include BWAPI::Client::Projects::Sharing
+      include BWAPI::Client::Projects::StreamDashboards
       include BWAPI::Client::Projects::Queries
       include BWAPI::Client::Projects::QueryGroups
       include BWAPI::Client::Projects::Tags
---
+++ lib/bwapi/client/projects/stream_dashboards.rb
@@ -0,0 +1,68 @@
+require 'bwapi/client/projects/stream_dashboards/streams'
+
+module BWAPI
+  class Client
+    module Projects
+      # StreamDashboards module for projects/stream_dashboards endpoints
+      module StreamDashboards
+
+        # Get all stream dashboards in project
+        #
+        # @param project_id [Integer] Id of project
+        # @param opts [Hash] options hash of parameters
+        # @option opts [Integer] page Page of projects to retrieve
+        # @option opts [Integer] pageSize Results per page of results
+        # @return [Hashie::Mash] All stream dashboards for project
+        def stream_dashboards project_id, opts={}
+          get "projects/#{project_id}/streamDashboards", opts
+        end
+
+        # Get a stream dashboard
+        #
+        # @param project_id [Integer] Id of project
+        # @param stream_dashboard_id [Integer] Id of stream dashboard
+        def get_stream_dashboard project_id, stream_dashboard_id
+          get "projects/#{project_id}/streamDashboards/#{stream_dashboard_id}"
+        end
+
+        # Create a new stream dashboard
+        #
+        # @param project_id [Integer] Id of project
+        # @param opts [Hash] options hash of parameters
+        # @option opts [Integer] id Id of the stream
+        # @option opts [String] name Name of the stream
+        # @option opts [String] created The stream creation date
+        # @option opts [String] filter the stream filter
+        # @return [Hashie::Mash] New stream dashboard
+        def create_stream_dashboard project_id, opts={}
+          post "projects/#{project_id}/streamDashboards", opts
+        end
+
+        # Update a stream dashboard
+        #
+        # @param project_id [Integer] Id of project
+        # @param stream_dashboard_id [Integer] Id of stream dashboard
+        # @param opts [Hash] options hash of parameters
+        # @option opts [Integer] id Id of the stream
+        # @option opts [String] name Name of the stream
+        # @option opts [String] created The stream creation date
+        # @option opts [String] filter the stream filter
+        # @return [Hashie::Mash] Updated stream dashboard
+        def update_stream_dashboard project_id, stream_dashboard_id
+          put "projects/#{project_id}/streamDashboards/#{stream_dashboard_id}"
+        end
+
+        # Delete a stream dashboard
+        #
+        # @param project_id [Integer] Id of project
+        # @param stream_dashboard_id [Integer] Id of stream dashboard
+        def delete_stream_dashboard project_id, stream_dashboard_id
+          delete "projects/#{project_id}/streamDashboards/#{stream_dashboard_id}"
+        end
+
+        include BWAPI::Client::Projects::StreamDashboards::Streams
+
+      end
+    end
+  end
+end
