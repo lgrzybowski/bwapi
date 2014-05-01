779cdf2c19257d5cf06f7f8d55af7c79b51ae5b1
Ben Slaughter <bens@brandwatch.com>
2014-05-01 17:31:12 +0100

    Added copy category endpoint

---
+++ lib/bwapi/client/projects/categories/copy.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+module BWAPI
+  class Client
+    module Projects
+      module Categories
+        # Mentions module for projects/bulkactions/mentions endpoints
+        module Copy
+          # Copy and existing category to another project
+          #
+          # @param project_id [Integer] Id of project
+          # @param category_id [Integer] Id of query group
+          # @param opts [Hash] options hash of parameters
+          # @option opts [Hash] destinationProjectId target project id for copy
+          # @return [Hashie::Mash] Copied category
+          def copy_category(project_id, category_id, opts = {})
+            post "projects/#{project_id}/categories/#{category_id}/copy", opts
+          end
+        end
+      end
+    end
+  end
+end
