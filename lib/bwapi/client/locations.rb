1b7edda1b3bde972f47caceffcbe33a22b1a443f
Ben Slaughter <bens@brandwatch.com>
2014-05-01 16:54:21 +0100

    Updated location endpoints to use the new locations endpoints

--- lib/bwapi/client/locations.rb
+++ lib/bwapi/client/locations.rb
@@ -3,23 +3,23 @@
 module BWAPI
   class Client
     # Location module for locations endpoints
-    module Location
+    module Locations
       # Request the information for a given location code
       #
       # @return [Hashie::Mash] Information about the location
-      def location(location_code)
+      def locations(location_code)
         get "location/#{location_code}"
       end
 
-      # Autocomplete a location name with candidates
+      # Locations a location name with candidates
       #
       # @param opts [String] prefix that the location starts with
       # @option opts [Array] a list of location types to return
       # @option opts [Integer] page number of results to retrieve
       # @option opts [Integer] pageSize of the results to retrieve
       # @return [Hashie::Mash] The list of matching locations
-      def location_autocomplete(opts = {})
-        post 'location/autocomplete', opts
+      def locations(opts = {})
+        post 'locations/autocomplete', opts
       end
     end
   end
