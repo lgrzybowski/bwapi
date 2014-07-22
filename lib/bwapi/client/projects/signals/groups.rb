10dc14b330d4e8adffeb37d1fb3c2d9f54c7b7a7
Preetamd <preetamd@brandwatch.com>
2014-07-22 15:48:42 +0100

    NoTask - Added all API requests for signals

--- lib/bwapi/client/projects/signals/groups.rb
+++ lib/bwapi/client/projects/signals/groups.rb
@@ -10,12 +10,44 @@ module BWAPI
           # Get all signals belonging to user in project
           #
           # @param project_id [Integer] Id of project
-          # @param opts [Hash] options hash of parameters
-          # @option opts [Integer] page Page of projects to retrieve
-          # @option opts [Integer] pageSize Results per page of results
           # @return [Hashie::Mash] All signal groups for project
           def signal_groups(project_id, opts = {})
             get "/projects/#{project_id}/signals/groups", opts
+          end
+
+          # Get specific signal group belonging to user in project
+          #
+          # @param project_id [Integer] Id of project
+          # @param group_id [Array] Id of group
+          # @return [Hashie::Mash] specific signal group
+          def get_signal_group(project_id, group_id)
+            get "/projects/#{project_id}/signals/group/#{group_id}"
+          end
+
+          # Get specific signal group belonging to user in project
+          #
+          # @param project_id [Integer] Id of project
+          # @return [Hashie::Mash] specific signal group
+          def create_signal_group(project_id, opts = {})
+            post "/projects/#{project_id}/signals/group", opts
+          end
+
+          # Get specific signal group belonging to user in project
+          #
+          # @param project_id [Integer] Id of project
+          # @param project_id [Array] Id of group
+          # @return [Hashie::Mash] specific signal group
+          def update_signal_group(project_id, group_id, opts = {})
+            put "/projects/#{project_id}/signals/groups/#{group_id}", opts
+          end
+
+          # Get specific signal group belonging to user in project
+          #
+          # @param project_id [Integer] Id of project
+          # @param project_id [Array] Id of group
+          # @return [Hashie::Mash] specific signal group
+          def delete_signal_group(project_id, group_id)
+            delete "/projects/#{project_id}/signals/groups/#{group_id}"
           end
         end
       end
