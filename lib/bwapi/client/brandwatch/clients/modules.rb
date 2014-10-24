7d3c72ac4f6a60de2a2a5dd9591696344a25b7c8
Lee McGeever <lee@brandwatch.com>
2014-10-24 11:51:34 +0100

    NoTask -update brandwatch client modules endpoint

---
+++ lib/bwapi/client/brandwatch/clients/modules.rb
@@ -0,0 +1,27 @@
+module BWAPI
+  class Client
+    module Brandwatch
+      # Clientmodules module for brandwatch/client/modules endpoints
+      module Clients
+        module Modules
+          # Get a specific clients modules
+          #
+          # @param client_id [Integer] Id of the client
+          # @return [Hash] Specific clients modules
+          def brandwatch_client_modules(client_id)
+            get "brandwatch/clients/#{client_id}/modules"
+          end
+
+          # Update brandwatch client modules
+          #
+          # @param opts [Hash] options Hash of parameters
+          # @option opts [Array] brandwatch/client/modules Client Modules to edit
+          # @return [Hash] Update client modules
+          def update_brandwatch_client_module(opts = {})
+            put "brandwatch/clients/#{client_id}/modules", opts
+          end
+        end
+      end
+    end
+  end
+end
