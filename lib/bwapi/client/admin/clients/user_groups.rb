aa333792637a7c7936762bd2c5c901cf06c8fd22
Jonathan Chrisp <jonathan@brandwatch.com>
2014-11-12 09:47:24 +0000

    158 - fixed small bug where options could not be passed - closes #158

--- lib/bwapi/client/admin/clients/user_groups.rb
+++ lib/bwapi/client/admin/clients/user_groups.rb
@@ -9,8 +9,9 @@ module BWAPI
           # @param client_id [Integer] Id of the client
           # @option opts [Integer] page Page of projects to retrieve
           # @option opts [Integer] pageSize Results per page of results
-          def client_user_groups(client_id)
-            get "admin/clients/#{client_id}/usergroups"
+          # @return [Hash] All client user groups
+          def client_user_groups(client_id, opts = {})
+            get "admin/clients/#{client_id}/usergroups", opts
           end
 
           # Create new user group
@@ -18,6 +19,7 @@ module BWAPI
           # @param client_id [Integer] Id of the client
           # @param opts [Hash] options hash of parameters
           # @option opts [Date] startDate Start date of the client
+          # @return [Hash] New client user group
           def create_client_user_group(client_id, opts = {})
             post "admin/clients/#{client_id}/usergroups", opts
           end
@@ -26,6 +28,7 @@ module BWAPI
           #
           # @param client_id [Integer] Id of the client
           # @param user_group_id [Integer] Id of the user group
+          # @return [Hash] Updated client user group
           def update_client_user_group(client_id, user_group_id, opts = {})
             put "admin/clients/#{client_id}/usergroups/#{user_group_id}", opts
           end
@@ -34,6 +37,7 @@ module BWAPI
           #
           # @param client_id [Integer] Id of the client
           # @param user_group_id [Integer] Id of the user group
+          # @return [Hash] Deleted client user group
           def delete_client_user_group(client_id, user_group_id)
             delete "admin/clients/#{client_id}/usergroups/#{user_group_id}"
           end
