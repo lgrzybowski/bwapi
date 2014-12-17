32883e0620e1d28aa4c65370398f52c9470d67b4
Ben Slaughter <bens@brandwatch.com>
2014-12-17 09:52:52 +0000

    NoTask - Added mentions facebookcomments and facebookposts endpoints

--- lib/bwapi/client/projects/data/mentions.rb
+++ lib/bwapi/client/projects/data/mentions.rb
@@ -61,6 +61,36 @@ module BWAPI
             get "projects/#{project_id}/data/mentions/tweets", opts
           end
 
+          # Get mentions that fall within the submitted filters for all facebook posts
+          #
+          # @param project_id [Integer] Id of project
+          # @param opts [Hash] options hash of parameters
+          # @option opts [Hash] filter The filters to apply
+          # @option opts [Integer] page Page of projects to retrieve
+          # @option opts [Integer] pageSize Results per page of results
+          # @option opts [String] orderBy Parameter to sort by
+          # @option opts [String] orderDirection Direction of sort
+          # @option opts [Integer] orderByCategory Category to sort by when orderBy category
+          # @return [Hash] All Mentions
+          def data_mentions_facebook_posts(project_id, opts = {})
+            get "projects/#{project_id}/data/mentions/facebookposts", opts
+          end
+
+          # Get mentions that fall within the submitted filters for all facebook comments
+          #
+          # @param project_id [Integer] Id of project
+          # @param opts [Hash] options hash of parameters
+          # @option opts [Hash] filter The filters to apply
+          # @option opts [Integer] page Page of projects to retrieve
+          # @option opts [Integer] pageSize Results per page of results
+          # @option opts [String] orderBy Parameter to sort by
+          # @option opts [String] orderDirection Direction of sort
+          # @option opts [Integer] orderByCategory Category to sort by when orderBy category
+          # @return [Hash] All Mentions
+          def data_mentions_facebook_comments(project_id, opts = {})
+            get "projects/#{project_id}/data/mentions/facebookcomments", opts
+          end
+
           include BWAPI::Client::Projects::Data::Mentions::Notes
         end
       end
