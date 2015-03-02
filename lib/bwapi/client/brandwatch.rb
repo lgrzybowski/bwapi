edc3786a82c919608fd94eec998a4a75f62ce2e1
Ben Slaughter <bens@brandwatch.com>
2015-03-02 17:13:13 +0000

    Added missing brandwatch commandcenter module

--- lib/bwapi/client/brandwatch.rb
+++ lib/bwapi/client/brandwatch.rb
@@ -1,5 +1,6 @@
 require 'bwapi/client/brandwatch/become'
 require 'bwapi/client/brandwatch/clients'
+require 'bwapi/client/brandwatch/command_center'
 require 'bwapi/client/brandwatch/ditto_brands'
 require 'bwapi/client/brandwatch/log_level'
 require 'bwapi/client/brandwatch/price_structures'
@@ -30,6 +31,7 @@ module BWAPI
 
       include BWAPI::Client::Brandwatch::Become
       include BWAPI::Client::Brandwatch::Clients
+      include BWAPI::Client::Brandwatch::CommandCenter
       include BWAPI::Client::Brandwatch::DittoBrands
       include BWAPI::Client::Brandwatch::LogLevel
       include BWAPI::Client::Brandwatch::PriceStructures
